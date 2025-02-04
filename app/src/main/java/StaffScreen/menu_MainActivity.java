23d3bbc138e123959245bf80d2995184a8b4bffd
Duy Hai <145411748+haitd2810@users.noreply.github.com>
2025-02-04 22:31:00 +0700

    feat: implement code UI

---
+++ app/src/main/java/StaffScreen/menu_MainActivity.java
@@ -0,0 +1,39 @@
+package StaffScreen;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project.R;
+
+import Common.navigationHelper;
+
+public class menu_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu_main);
+
+        navigationHelper.setUpNavigation(this);
+
+        findViewById(R.id.cartIcon).setOnClickListener(v -> {
+            Intent intent = new Intent(this, order_MainActivity.class);
+            startActivity(intent);
+        });
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/StaffScreen/order_MainActivity.java
@@ -0,0 +1,42 @@
+package StaffScreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project.R;
+
+import Common.navigationHelper;
+
+public class order_MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_order_main);
+
+        navigationHelper.setUpNavigation(this);
+
+        findViewById(R.id.addOrderButton).setOnClickListener(v -> {
+            Intent intent = new Intent(this, menu_MainActivity.class);
+            startActivity(intent);
+        });
+
+        findViewById(R.id.btnOrder).setOnClickListener( v -> {
+            Intent intent = new Intent(this, detail_MainActivity.class);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
