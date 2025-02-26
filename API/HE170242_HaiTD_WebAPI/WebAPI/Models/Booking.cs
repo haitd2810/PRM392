eb10a1a3abfa8d5af29221e7c4964a246491a2c5
Duy Hai <145411748+haitd2810@users.noreply.github.com>
2025-02-26 22:48:07 +0700

    feat: implement API and test for menu API

---
+++ API/HE170242_HaiTD_WebAPI/WebAPI/Models/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebAPI.Models
+{
+    public partial class Booking
+    {
+        public int Id { get; set; }
+        public int? TableId { get; set; }
+        public DateTime? StartDate { get; set; }
+        public string? Status { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? FullName { get; set; }
+        public DateTime? CreateAt { get; set; }
+        public int? NumberOfBooking { get; set; }
+
+        public virtual Table? Table { get; set; }
+    }
+}
---
+++ Android/app/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.project"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.project"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation("com.microsoft.sqlserver:mssql-jdbc:10.2.1.jre8")
+    implementation("net.sourceforge.jtds:jtds:1.3.1")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
